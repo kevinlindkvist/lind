90b8513ac65aed33faedabe0028ad4c3d93732a4
Kevin Lindkvist <kevin.lindkvist@gmail.com>
2016-08-28 23:30:24 -0700

    Initial Commit

---
+++ lind/lind.h
@@ -0,0 +1,19 @@
+//
+//  lind.h
+//  lind
+//
+//  Created by Kevin Lindkvist on 8/28/16.
+//  Copyright © 2016 lindkvist. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for lind.
+FOUNDATION_EXPORT double lindVersionNumber;
+
+//! Project version string for lind.
+FOUNDATION_EXPORT const unsigned char lindVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <lind/PublicHeader.h>
+
+
