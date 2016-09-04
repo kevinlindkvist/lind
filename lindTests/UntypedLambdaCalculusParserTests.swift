7a143d79ffcaa3fef253d960cc07f535dc13d017
Kevin Lindkvist <kevin.lindkvist@gmail.com>
2016-09-04 00:35:22 -0700

    Add more tests for untyped lc parsing.

--- lindTests/UntypedLambdaCalculusParserTests.swift
+++ lindTests/UntypedLambdaCalculusParserTests.swift
@@ -33,41 +33,53 @@ class UntypedLambdaCalculusParserTests: XCTestCase {
     testParseResult("x", expected: expected)
   }
 
-  func testabs() {
+  func testAbs() {
     let expected: (NamingContext, LCTerm) = (Dictionary<String, Int>(), .abs("x", .va("x", 0)))
     testParseResult("\\x.x", expected: expected)
   }
 
-  func testabsabs() {
-    let expected: (NamingContext, LCTerm) = (Dictionary<String, Int>(), .abs("x", .abs("y", .app(.va("x", 1), .app(.va("y", 0), .va("x", 1))))))
-    testParseResult("\\x.\\y.x (y x)", expected: expected)
+  func testAbsAbs() {
+    let expected: (NamingContext, LCTerm) = (["y": 1, "x": 0], .app(.abs("x", .abs("y", .app(.va("x", 1), .va("y", 0)))), .app(.va("x", 0), .va("y", 1))))
+    testParseResult("\\x.\\y.(x y) (x y)", expected: expected)
   }
 
-  func testapp() {
+  func testApp() {
     let expected: (NamingContext, LCTerm) = (["y": 0, "z": 1], .app(.abs("x", .va("y", 1)), .va("z", 1)))
     testParseResult("\\x.y z", expected: expected)
   }
 
-  func testappTwice() {
+  func testAppTwice() {
     let lhs: LCTerm = .app(.abs("x", .va("y", 1)), .va("z", 1))
     let expected: LCTerm =  .app(lhs, .va("d", 2))
     testParseResult("\\x.y z d", expected: (["y": 0, "z": 1, "d": 2], expected))
   }
 
 
-  func testappParens() {
+  func testAppParens() {
     let lhs: LCTerm = .app(.abs("x", .va("x", 0)), .va("d", 0))
     let rhs: LCTerm = .app(.abs("z", .va("z", 0)), .va("l", 1))
     testParseResult("(\\x.x d) (\\z.z l)", expected: (["d": 0, "l": 1], .app(lhs, rhs)))
   }
 
-  func testappAssociativity() {
+  func testDeBruijn() {
+    let lhs: LCTerm = .abs("x", .app(.va("x", 0), .va("d", 1)))
+    let rhs: LCTerm = .app(.abs("z", .va("z", 0)), .va("l", 1))
+    testParseResult("(\\x.(x d)) (\\z.z l)", expected: (["d": 0, "l": 1], .app(lhs, rhs)))
+  }
+
+  func testSubstitution() {
+    let lhs: LCTerm = .abs("y", .app(.va("x", 1), .va("y", 0)))
+    let rhs: LCTerm = .app(.va("y", 1), .va("z", 2))
+    testParseResult("(\\y.(x y)) (y z)", expected: (["x": 0, "y": 1, "z": 2], .app(lhs, rhs)))
+  }
+
+  func testAppAssociativity() {
     let lhs: LCTerm = .app(.va("a", 0), .va("b", 1))
     let expected: LCTerm = .app(lhs, .va("c", 2))
     testParseResult("a b c", expected: (["a": 0, "b": 1, "c": 2], expected))
   }
 
-  func testappAssociativityParens() {
+  func testAppAssociativityParens() {
     let rhs: LCTerm = .app(.va("b", 1), .va("c", 2))
     let expected: LCTerm = .app(.va("a", 0), rhs)
     testParseResult("a (b c)", expected: (["a": 0, "b": 1, "c": 2], expected))
